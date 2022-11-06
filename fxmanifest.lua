55a4c413d69d501938d62f21de7cb9c403456c65
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-11-06 12:11:35 +1100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+authors {'Rio', 'DokaDoka'}
+
+files {
+    'data/*.meta',
+}
+
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'

d4013bf4ce32109dcff9e7d5a62b196d503c803c
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-12-10 01:06:15 +1100

    fix(fxmanifest): carcols files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,7 +15,10 @@ files {
     'data/*.meta',
 }
 
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CARCOLS_FILE' 'data/carcols-kits1.meta'
+data_file 'CARCOLS_FILE' 'data/carcols-kits2.meta'
+data_file 'CARCOLS_FILE' 'data/carcols-kits3.meta'
+data_file 'CARCOLS_FILE' 'data/carcols-other.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
